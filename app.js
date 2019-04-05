4986aee8ec5356af3414d8681b9ea7516b57cbee
J.U.N <un@JUNurcans-MacBook.local>
2019-04-05 17:46:41 -0400

    working version

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-console.log("hello");
-
+"use strict";
 const myform = document.querySelector("form");
 const inputs = document.querySelectorAll("input");
-const errors = document.querySelectorAll(".errors");
+const errors = document.querySelectorAll(".error");
 const required = ["email", "username"];
 
 // preventing submit button to refresh the page
@@ -10,12 +9,17 @@ myform.addEventListener("submit", validate);
 
 function validate(e) {
   e.preventDefault();
-  let data = {};
+  let err = false;
+  console.log("err1", err);
+
+  errors.forEach(item => item.classList.add("hide"));
+  document
+    .querySelectorAll(".error-border")
+    .forEach(item => item.classList.remove("error-border"));
+
   inputs.forEach(el => {
     let nameProp = el.getAttribute("name");
-    let err = false;
     if (nameProp !== null) {
-      data[nameProp] = el.value;
       if (el.value.length === 0 && required.includes(nameProp)) {
         addError(el, "Required Field", nameProp);
         err = true;
@@ -23,15 +27,31 @@ function validate(e) {
       if (nameProp === "email") {
         let exp = /([A-Za-z0-9._-]+@[A-Za-z0-9._-]+\.[A-Za-z0-9]+)\w+/;
         let result = exp.test(el.value);
-        if (!result) console.log("not valid email addres");
-        err = true;
+        if (!result) addError(el, "not valid email addres", nameProp);
+        // err = true;
+      }
+
+      if (nameProp === "password") {
+        let exp = /[A-Za-z0-9]+$/;
+        let result = exp.test(el.value);
+        if (!result) {
+          addError(el, "Only numbers and letters", nameProp);
+          err = true;
+        }
+        if (!(el.value.length > 3 && el.value.length < 9)) {
+          addError(el, "Must be between 3 and 8 characters", nameProp);
+          err = true;
+        }
       }
     }
-
-    console.log(el.value);
   });
-
-  console.log(data);
+  console.log(err);
+  if (!err) {
+    document.querySelector("form").classList.add("hide");
+    let h1 = document.createElement("H1");
+    h1.textContent = "Thanks. Form has been submitted";
+    document.body.appendChild(h1);
+  }
 }
 
 function addError(el, msg, nameProp) {
